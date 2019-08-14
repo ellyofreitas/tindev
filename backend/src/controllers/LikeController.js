b8304792783e56e90671a2fd4eca904bce389fa6
Ellyo Freitas <ellyofreitas@localhost.localdomain>
2019-08-14 01:32:32 -0300

    finalizado frontend

---
+++ backend/src/controllers/LikeController.js
@@ -0,0 +1,29 @@
+const Dev = require('../models/Dev');
+
+module.exports = {
+  async store(req, res) {
+    const { user } = req.headers;
+    const { devId } = req.params;
+
+    const loggedDev = await Dev.findById(user);
+    const targetDev = await Dev.findById(devId);
+
+    if (!targetDev) {
+      return res.status(400).json({ error: 'Dev not exists' });
+    }
+
+    if (targetDev.likes.includes(loggedDev._id)) {
+      console.log('DEU MATCH');
+    }
+
+    if (loggedDev.likes.includes(targetDev._id)) {
+      return res.json(loggedDev);
+    }
+
+    loggedDev.likes.push(targetDev._id);
+
+    await loggedDev.save();
+
+    return res.json(loggedDev);
+  }
+};
